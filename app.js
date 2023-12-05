6dad6977b72883225f8f74fb339526ab56400e69
minwoopak <alsdn9626@gmail.com>
2023-12-06 01:21:15 +0900

    231206 yelpcamp

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
-const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -13,6 +13,8 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+const app = express();
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
